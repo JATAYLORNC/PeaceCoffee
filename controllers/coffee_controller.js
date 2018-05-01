cf05187138809d656ef3954d86af168ea61befd5
JATAYLORNC <disneyjeff@gmail.com>
2018-05-01 12:02:32 -0400

    committing initial code framework

---
+++ controllers/coffee_controller.js
@@ -0,0 +1,37 @@
+//define dependencies
+var express = require("express");
+var db = require("../models");
+
+// Routes
+// =============================================================
+module.exports = function(app) {
+
+  app.get("/", function (req, res) {
+
+  
+  });
+
+  
+  app.post("/api/member", function(req, res) {
+
+
+  });
+
+  app.post("/api/customer", function(req, res) {
+
+    
+  });
+
+
+  app.put("/api/member/:id", function(req, res) {
+
+    
+  });
+
+  app.delete("api/member/:id", function(req, res) {
+
+
+  });
+
+
+};
