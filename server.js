570fed1b18952647defbefd266326b7537bd1a6c
JATAYLORNC <disneyjeff@gmail.com>
2018-05-05 15:25:21 -0400

    added html, css, and js files to 'public' directory

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 //Requiring necessary npm packages
 var express = require("express");
+var path = require("path");
 var bodyParser = require("body-parser");
 var session = require("express-session");
-//Requring passport as we've configured it
-var passport = require("./config/passport");
 
 require("dotenv").config();
 
 //create instance of express server and define port
 var app = express();
 var port = process.env.PORT || 3000;
+
+//Requring passport as we've configured it
+// var passport = require("./config/passport");
+
+// We need to use sessions to keep track of our user's login status
+// app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+// app.use(passport.initialize());
+// app.use(passport.session());
 
 //requiring our models for syncing
 var db = require("./models");
@@ -20,11 +27,6 @@ app.use(bodyParser.json());
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
-
-// We need to use sessions to keep track of our user's login status
-app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
-app.use(passport.initialize());
-app.use(passport.session());
 
 //dependency for node express handlebars
 var exphbs = require("express-handlebars");
@@ -37,7 +39,7 @@ app.set("view engine", "handlebars");
 // =============================================================
 require("./controllers/peace_controller.js")(app);
 require("./controllers/coffee_controller.js")(app);
-require("./controllers/auth_controller.js")(app);
+// require("./controllers/auth_controller.js")(app);
 
 //sync the models
 db.sequelize.sync({force: true}).then(function() {
